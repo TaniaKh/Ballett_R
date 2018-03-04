74da4ffd138a008598c6784a801deefcb7ffd36d
TaniaKh <34840985+TaniaKh@users.noreply.github.com>
2018-03-04 20:53:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tania/ballett_studio_project_one/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tania.ballett_studio_project_one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
